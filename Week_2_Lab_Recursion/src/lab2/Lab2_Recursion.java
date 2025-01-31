b68720880fa21d8ba3ba91c16a40c616126ffb89
tehepix2 <daniel12landa@gmail.com>
2025-01-31 09:25:24 -0500

    Recursion practice with summation

---
+++ Week_2_Lab_Recursion/src/lab2/Lab2_Recursion.java
@@ -0,0 +1,39 @@
+package lab2;
+
+public class Lab2_Recursion {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        System.out.println("Calculated using a formula: " + triangularFormula(5)); // * This uses the n(n+1)/2 formula
+
+        System.out.println("Calculated using a loop: " + triangularLoop(5)); // * This uses a for loop to calculate it
+
+        System.out.println("Calculated using recursion: " + triangularRecursion(5)); // * This uses recursion
+
+        System.out.println("Factorial calculated using recursion: " + factorialRecursion(5));
+
+
+    }
+
+    public static int triangularFormula(int n) {
+        return (n * (n + 1)) / 2;
+    }
+
+    public static int triangularLoop(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static int triangularRecursion(int n) {
+        if (n == 1) { return 1; }
+        return n + triangularRecursion(n - 1);
+    }
+
+    public static int factorialRecursion(int n) {
+        if (n == 1) { return 1; }
+        return n * factorialRecursion(n - 1);
+    }
+}
