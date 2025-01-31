eb4a281c308ea2df5706871b0832cfc322a7e7e4
tehepix2 <daniel12landa@gmail.com>
2025-01-31 09:40:29 -0500

    did some extra recursive practice

--- Week_2_Lab_Recursion/src/lab2/Lab2_Recursion.java
+++ Week_2_Lab_Recursion/src/lab2/Lab2_Recursion.java
@@ -10,9 +10,9 @@ public class Lab2_Recursion {
 
         System.out.println("Calculated using recursion: " + triangularRecursion(5)); // * This uses recursion
 
-        System.out.println("Factorial calculated using recursion: " + factorialRecursion(5));
-
-
+        System.out.println("Factorial calculated using recursion: " + factorialRecursion(5)); // * This calculates the factorial by using recursion
+        
+        System.out.println("Exponentiation using recursion: " + exponentRecursion(5, 3)); // * This calculates a base to the power of an exponent by using recursion
     }
 
     public static int triangularFormula(int n) {
@@ -21,19 +21,23 @@ public class Lab2_Recursion {
 
     public static int triangularLoop(int n) {
         int sum = 0;
-        for (int i = 1; i <= n; i++) {
-            sum += i;
-        }
+        for (int i = 1; i <= n; i++) {sum += i;}
         return sum;
     }
 
     public static int triangularRecursion(int n) {
-        if (n == 1) { return 1; }
+        System.out.println("One loop");
+        if (n == 1) {return 1;}
         return n + triangularRecursion(n - 1);
     }
 
-    public static int factorialRecursion(int n) {
-        if (n == 1) { return 1; }
+    public static int factorialRecursion(int n) { // * Some extra practice
+        if (n == 1) {return 1;}
         return n * factorialRecursion(n - 1);
+    }
+
+    public static int exponentRecursion(int base, int power) { // * Some extra practice
+        if (power == 0) {return 1;}
+        return base * exponentRecursion(base, power - 1);
     }
 }
